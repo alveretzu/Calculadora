fadf73c15bf2eb52d1e37cc24fa6cab84cf96fae
freddydiaz <freddysuriel21@hotmail.com>
2015-02-15 15:20:46 -0600

    calculadora
    funciones

--- freddy.py
+++ freddy.py
@@ -1,1 +1,12 @@
-print "hoal mundo"
+def suma(x,y):
+	valor=x+y
+	print valor
+def resta(x,y):
+	valor=x-y
+	print valor
+def multiplicacion(x,y):
+	valor=x*y
+	print valor
+def division(x,y):
+	valor=x/y
+	print valor
